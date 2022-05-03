6b3bbfd23fe666bfc4e0bee00190ab10d75cc5f7
Petteri Peltonen <petteri.peltonen@vtt.fi>
2022-05-03 14:09:26 +0300

    removed stuff

--- src/convection_schemes/convection_scheme.cpp
+++
@@ -1,1 +0,0 @@
-#include "convection_scheme.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,7 @@
-#include "driver.hpp"
 #include <iostream>
 
 int main() {
 
-    // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
-
-    Driver driver = Driver();
-
-    driver.run();
-
-    // Finalize the MPI environment.
-    MPI_Finalize();
 
     return 0;
 }
