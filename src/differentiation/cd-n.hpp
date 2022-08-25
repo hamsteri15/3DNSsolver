156f68ee52c71c1b83e7b37cb4329201089424a7
petteri <petteri.peltonen@vtt.fi>
2022-08-25 11:32:57 +0300

    small

--- src/differentiation/cd-n.hpp
+++ src/differentiation/cd-n.hpp
@@ -3,22 +3,15 @@
 #include "common/mdspan.hpp"
 #include "differentiation/tiled_stencil.hpp"
 
+template <size_t Dir> struct d_CD2 : public TiledStencil<d_CD2<Dir>> {
 
-
-
-template<size_t Dir>
-struct d_CD2 : public TiledStencil<d_CD2<Dir>>{
-
-
-    static constexpr size_t padding = 1;
+    static constexpr size_t padding   = 1;
     static constexpr size_t direction = Dir;
 
-
-    auto operator()(auto span, auto idx) const{
-        return
-        (span(shift<Dir, 1>(idx))-span(shift<Dir, -1>(idx)));;
+    auto operator()(auto span, auto idx) const {
+        return (span(shift<Dir, 1>(idx)) - span(shift<Dir, -1>(idx)));
+        ;
     }
-
 };
 
 template<size_t Dir>
