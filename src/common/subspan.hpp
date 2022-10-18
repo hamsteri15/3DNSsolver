25676fe7c38462c32b15788974d28ca50e3c4ac5
Petteri Peltonen <petteri.peltonen@vtt.fi>
2022-10-18 14:40:17 +0300

    small

--- src/common/subspan.hpp
+++ src/common/subspan.hpp
@@ -23,14 +23,14 @@ make_subspan(span<T, N> s, std::array<size_t, N> begin, std::array<size_t, N> en
 
 namespace detail {
 
-template <size_t Dir, size_t I> struct Asd {
+template <size_t Dir, size_t I> struct SubspanParams {
     static constexpr auto process(auto a, auto b) {
         (void)b;
         return std::get<I>(a);
     }
 };
 
-template <size_t K> struct Asd<K, K> {
+template <size_t K> struct SubspanParams<K, K> {
     static constexpr auto process(auto a, auto b) {
         return std::make_pair(std::get<K>(a), std::get<K>(b));
     }
@@ -40,7 +40,7 @@ template <size_t Dir, size_t N>
 auto make_tiled_subspan_params(std::array<size_t, N> a, std::array<size_t, N> b) {
 
     auto make_tuple = [=]<size_t... Is>(std::index_sequence<Is...>) {
-        return std::make_tuple(Asd<Dir, Is>::process(a, b)...);
+        return std::make_tuple(SubspanParams<Dir, Is>::process(a, b)...);
     };
 
     return make_tuple(std::make_index_sequence<N>());
