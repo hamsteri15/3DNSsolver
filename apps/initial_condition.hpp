005cfcffbec8f4003e43b08f94abd86d6994d98d
petteri <petteri.peltonen@vtt.fi>
2022-11-07 12:06:22 +0200

    initial conditions

--- apps/initial_condition.hpp
+++ apps/initial_condition.hpp
@@ -2,32 +2,48 @@
 
 #include "equation/euler.hpp"
 
-template<size_t Dir, size_t N>
-void assign_shocktube(Euler<N>& eq){
+void for_each_index(auto span, auto op){
+    for (auto idx : all_indices(span)){
+        span(tuple_to_array(idx)) = op(tuple_to_array(idx));
+    }
+}
 
+template<size_t N>
+void assign_condition(Euler<N>& eq, auto rho_op, auto p_op, auto U_op){
 
     auto& prim = eq.primitive_variables();
-    auto dims = full_extent(prim.rho);
-
     auto rho = make_full_span(prim.rho);
     auto p = make_full_span(prim.p);
     auto U = make_full_span(prim.U);
 
-    for (auto idx : all_indices(rho)){
+    for_each_index(rho, rho_op);
+    for_each_index(p, p_op);
+    for_each_index(U, U_op);
+}
 
-        auto ii = tuple_to_array(idx);
-        
-        if (std::get<Dir>(ii) < size_t(0.5*dims.extent(Dir))){
-            
-            rho(ii) = 1.0;
-            p(ii) = 1.0;
-            U(ii) = Vector<N>{};
-        }
-        else{
-            rho(ii) = 0.125;
-            p(ii) = 0.1;
-            U(ii) = Vector<N>{};
-        }
 
-    }
+template<size_t Dir, size_t N>
+void assign_shocktube(Euler<N>& eq){
+
+    auto dims = full_extent(eq.primitive_variables().rho);
+
+    auto rho_op = [=](auto i){
+        if (std::get<Dir>(i) < size_t(0.5*dims.extent(Dir))){
+            return 1.0;
+        }
+        return 0.125;
+    };
+    auto p_op = [=](auto i){
+        if (std::get<Dir>(i) < size_t(0.5*dims.extent(Dir))){
+            return 1.0;
+        }
+        return 0.1;
+    };
+    auto U_op = [=](auto i){
+        (void) i;
+        return Vector<N>{};
+    };
+
+    assign_condition(eq, rho_op, p_op, U_op);
+
 }
